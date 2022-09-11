6b0e4efab6774455d8d6e7fe42ad5000ee77981f
mario schmoker <marioschmoker@tdf.edu.ar>
2022-09-11 12:02:42 -0300

    terminado

---
+++ holaMundo.js
@@ -0,0 +1,8 @@
+console.log("hola mundo");
+
+let readlineSync = require("readline-sync");
+
+let alturaPersona = readlineSync.question("Indique la altura de la persona: ");
+
+console.log(alturaPersona);
+
